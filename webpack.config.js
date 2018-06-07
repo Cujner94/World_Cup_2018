06278a822add1a3b6b0c33b854b2fa1b280931c2
Cujner <Cujner>
2018-06-07 15:40:04 +0200

    First

---
+++ webpack.config.js
@@ -0,0 +1,23 @@
+const webpack = require("webpack");
+const path    = require("path");
+
+const config = {
+	
+	entry: './src/index.jsx',
+	output: {
+		path: path.resolve(__dirname, 'dist'),
+		filename: 'bundle.js'
+	},
+	module:{
+		rules: [
+			{
+				test: /\.(js|jsx)$/,
+				exclude: /node_modules/,
+				use: 'babel-loader'
+			}
+		]
+	}
+	
+}
+
+module.exports = config;
