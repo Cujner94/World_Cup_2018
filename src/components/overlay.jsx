33c4c5dfbc11428b9c94a8749d621a725dc17776
Cujner <Cujner>
2018-06-12 20:50:02 +0200

    Made Overlay Statless component

--- src/components/overlay.jsx
+++ src/components/overlay.jsx
@@ -2,21 +2,6 @@ import React from 'react';
 import Players from './players.jsx';
 import Fixtures from './fixtures.jsx'
 
-// class Overlay extends React.Component{
-	
-// 	render(){
-// 		return(
-// 			<div onClick={this.props.toggleOverlay} className="overlay">
-// 				<div className="overlay_inner">
-// 					<button className="overlay-close" onClick={this.props.toggleOverlay}>X</button>
-// 					{this.props.players && <Players players={this.props.players}/>}
-// 					{this.props.fixtures && <Fixtures data={this.props.fixtures} />}
-// 				</div>
-// 			</div>
-// 		)
-// 	}
-// }
-
 const Overlay = (props) => (
 	<div onClick={props.toggleOverlay} className="overlay">
 		<div className="overlay_inner">
