542ee90a942a36bcbd967b484291275c12e26efd
Cujner <Cujner>
2018-06-12 20:51:37 +0200

    Made MainList stateless component

--- src/components/mainlist.jsx
+++ src/components/mainlist.jsx
@@ -1,20 +1,23 @@
 import React from 'react';
 
-class MainList extends React.Component{
+// class MainList extends React.Component{
 	
-	logData = () =>{
-		console.log(this.props.data);
-	}
-	
-	render(){
-		return(
-			<div className="container-div main-container">
-				<h1 className="main-title">FIFA WORLD CUP</h1>
-				<h1 className="main-year">2018 - RUSSIA</h1>
-			</div>
-		)
-	}
-}
+// 	render(){
+// 		return(
+// 			<div className="container-div main-container">
+// 				<h1 className="main-title">FIFA WORLD CUP</h1>
+// 				<h1 className="main-year">2018 - RUSSIA</h1>
+// 			</div>
+// 		)
+// 	}
+// }
+
+const MainList = props => (
+	<div className="container-div main-container">
+		<h1 className="main-title">FIFA WORLD CUP</h1>
+		<h1 className="main-year">2018 - RUSSIA</h1>
+	</div>
+)
 
 
 export default MainList;
